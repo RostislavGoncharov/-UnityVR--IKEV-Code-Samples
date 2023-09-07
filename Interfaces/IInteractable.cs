2ba27e3bba2374c2b764416e06bbf8bb9038864d
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2023-09-07 14:16:30 +0300

    add OnInteractionFinished, OnHover and OnHoverFinished functions to IInteractable interface

--- Interfaces/IInteractable.cs
+++ Interfaces/IInteractable.cs
@@ -1,5 +1,9 @@
 using UnityEngine.InputSystem;
 public interface IInteractable
 {
+    string UIprompt { get; set; }
     public void OnInteract(InputAction.CallbackContext context);
+    public void OnInteractionFinished();
+    public void OnHover();
+    public void OnHoverFinished();
 }
