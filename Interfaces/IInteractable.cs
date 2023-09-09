b3657d22e971ec37c29595aae277fa310ec76604
Rostislav Goncharov <rgoncharov2010@gmail.com>
2023-09-09 12:34:22 +0300

    add comments for interface files

--- Interfaces/IInteractable.cs
+++ Interfaces/IInteractable.cs
@@ -1,4 +1,10 @@
 using UnityEngine.InputSystem;
+
+/*
+ * This interface declares the behavior of all interactable objects.
+ * The UIprompt property is used to update the prompt appearing over the controller
+ * when an interaction is available.
+ */
 public interface IInteractable
 {
     string UIprompt { get; set; }
