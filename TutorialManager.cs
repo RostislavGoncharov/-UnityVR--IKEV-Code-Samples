5a014c48f157971a8f295140cff4e8c017c9b760
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2023-09-13 16:16:15 +0300

    add check for test environment

--- TutorialManager.cs
+++ TutorialManager.cs
@@ -30,7 +30,7 @@ public class TutorialManager : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance != null && Instance != this)
+        if ((Instance != null && Instance != this) || GameManager.Instance.isTestEnv)
         {
             Destroy(this);
         }
