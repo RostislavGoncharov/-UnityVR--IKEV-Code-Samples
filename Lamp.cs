865f9ea04f1b4e591c24ccb4c1041c60f868d2b9
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2023-10-25 13:21:57 +0300

    implement lamp functions

---
+++ Lamp.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Lamp : XRGrabInteractableExtraAttach
+{
+    [SerializeField] Light pointLight;
+    [SerializeField] Light spotLight;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        UIprompt = "Grip: Grab Lamp\nTrigger: Switch Light On/Off";
+
+        pointLight.enabled = false;
+        spotLight.enabled = false;
+    }
+    public override void OnInteract(InputAction.CallbackContext context)
+    {
+        base.OnInteract(context);
+
+        if (context.action.name == "InteractLeft" || context.action.name == "InteractRight")
+        {
+            SwitchLight();
+        }
+    }
+
+    void SwitchLight()
+    {
+        pointLight.enabled = !pointLight.enabled;
+        spotLight.enabled = !spotLight.enabled;
+    }
+}
