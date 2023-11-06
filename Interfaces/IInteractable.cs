d4559270b299c26be1b928a9399deb39c38ee837
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2023-11-06 13:09:08 +0200

    add CallbackContext

--- Interfaces/IInteractable.cs
+++ Interfaces/IInteractable.cs
@@ -9,7 +9,7 @@ public interface IInteractable
 {
     string UIprompt { get; set; }
     public void OnInteract(InputAction.CallbackContext context);
-    public void OnInteractionFinished();
+    public void OnInteractionFinished(InputAction.CallbackContext context);
     public void OnHover();
     public void OnHoverFinished();
 }
