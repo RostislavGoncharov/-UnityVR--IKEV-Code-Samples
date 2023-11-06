e9fd300ffb83e1354f60eaa5ac0c5bbbf5bc6a3d
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2023-11-06 13:18:54 +0200

    remove CallbackContext

--- Interfaces/IInteractable.cs
+++ Interfaces/IInteractable.cs
@@ -9,7 +9,7 @@ public interface IInteractable
 {
     string UIprompt { get; set; }
     public void OnInteract(InputAction.CallbackContext context);
-    public void OnInteractionFinished(InputAction.CallbackContext context);
+    public void OnInteractionFinished();
     public void OnHover();
     public void OnHoverFinished();
 }
