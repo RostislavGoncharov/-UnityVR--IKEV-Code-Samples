d0473c5901ed59a37f1dcad1a90fb89eb0bfc135
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2023-11-06 12:20:25 +0200

    implement IBlinking interface

--- StoolBox.cs
+++ StoolBox.cs
@@ -1,7 +1,50 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class StoolBox : Box
+public class StoolBox : Box, IBlinking
 {
+    [SerializeField] Material blinkMaterial;
+
+    MeshRenderer _meshRenderer;
+    List<Material> _materials = new List<Material>();
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (!TryGetComponent<MeshRenderer>(out _meshRenderer))
+        {
+            Debug.Log("Box: No MeshRenderer found");
+        }
+
+        Blink(true);
+    }
+    public void Blink(bool shouldBlink)
+    {
+        if (shouldBlink)
+        {
+            _meshRenderer.GetMaterials(_materials);
+            _meshRenderer.materials = new Material[] { _materials[0], blinkMaterial };
+        }
+        else
+        {
+            // Remove the blinking material if it's present    
+            _meshRenderer.GetMaterials(_materials);
+
+            if (_materials.Count > 1)
+            {
+                _meshRenderer.materials = new Material[] { _materials[0] };
+            }
+        }
+    }
+    public override void OnInteract(InputAction.CallbackContext context)
+    {
+        base.OnInteract(context);
+
+        Blink(false);
+    }
+
     protected override void OpenBox()
     {
         base.OpenBox();
