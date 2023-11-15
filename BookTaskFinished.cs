455b5a916ca99700be02b709470d0312d5637a46
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2023-11-15 14:00:11 +0200

    add comment

--- BookTaskFinished.cs
+++ BookTaskFinished.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 
+/*
+ * This class progresses the game as soon as the book is put in place.
+ * This script should be attached to the Books Assembled prefab.
+ */
+
 public class BookTaskFinished : MonoBehaviour
 {
     private void OnEnable()
