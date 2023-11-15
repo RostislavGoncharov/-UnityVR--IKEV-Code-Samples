2f37a73cde2ce98ef4ad137db4fb9b70146823ae
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2023-11-15 13:50:27 +0200

    add comment

--- Bed.cs
+++ Bed.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * This class progresses the game as soon as the bed is spawned.
+ * It should be attached to the Bed Assembled prefab.
+ */
 public class Bed : MonoBehaviour
 {
     private void Awake()
